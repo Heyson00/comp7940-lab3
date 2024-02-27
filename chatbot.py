86d1ec1af57e4b72d39dca2a9432feb379207025
krammer <931023602@qq.com>
2024-02-27 15:04:53 +0800

    modify

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,25 @@
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 from ChatGPT_HKBU import HKBU_ChatGPT
-import configparser
+import os
+# import configparser
 import logging
 import redis
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(os.environ['TELEGRAM_ACCESS_TOKEN']), use_context = True)
     dispatcher = updater.dispatcher
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-    password=(config['REDIS']['PASSWORD']),
-    port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['REDIS_HOST']))
+    password=(os.environ['REDIS_PASSWORD'])
+    port = (os.environ['REDISPORT'])
+    # redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+    # password=(config['REDIS']['PASSWORD']),
+    # port=(config['REDIS']['REDISPORT']))
     # You can set this logging module, so you will know when
     # and why things do not work as expected Meanwhile, update your config.ini as:
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
